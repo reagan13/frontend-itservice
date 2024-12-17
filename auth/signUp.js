ac4ef91eb9643473b1eead7ce44057fee4c3aef6
reagan13 <reaganmabawad@gmail.com>
2024-12-17 17:09:25 +0800

    update

--- auth/signUp.js
+++ auth/signUp.js
@@ -116,7 +116,7 @@ document.addEventListener("DOMContentLoaded", () => {
 				window.location.hostname === "localhost" ||
 				window.location.hostname === "127.0.0.1"
 					? "http://localhost:3000/api/signup"
-					: "https://backend-itservice.onrender.com/api/signin";
+					: "https://backend-itservice.onrender.com/api/signup";
 
 			// Send signup request
 			const response = await fetch(url, {
